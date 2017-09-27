83a8055680a8db6d39508adfb7bce99cf572da4e
linneasandelin <linnea.sandelin@nuway.se>
2017-09-27 09:00:01 +0200

    search results from api, listening on key up/down/enter and mouseenter/click, working version

--- search/Gruntfile.js
+++ search/Gruntfile.js
@@ -1,69 +1,63 @@
 module.exports = function(grunt) {
-        grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-    
-        watch: {
-            sass:{
-                files: ['assets/styles/*.scss'],
-                tasks: ['sass']
-            },
-            options: {
-                livereload: {
-                    host: 'localhost',
-                    files: [
-                      './index.html',
-                      './assets/styles/*.scss',
-                      './assets/js/*.js'
-                    ],
-                    open: true
-                }
-            }
+    grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+        sass:{
+            files: ['assets/styles/*.scss'],
+            tasks: ['sass']
         },
-    
-        sass: {
-            dist: {
-                files: {
-                    'dist/build/styles/style.css' : 'assets/styles/base.scss'
-                }
-            }
-        },
-    
-        concat: {
-            dist: {
-                src: ['assets/js/*.js'],
-                dest: 'dist/build/scripts/main.min.js'
-            }
-        },
-    
         uglify:{
-            options: {
-                manage: false,
-                preserveComments: 'all'
-            },
-            my_target:{
-                files: {
-                    'dist/build/scripts/main.min.js' : ['assets/js/*.js']
-                }
-            }
+            files: ['assets/js/*.js'],
+            tasks: ['uglify']
         },
-        copy: {
-            main: {
-                expand: true,
-                cwd: 'assets/img/',
-                src: '*.{png,jpg,svg}',
-                dest: 'dist/build/images'
+        options: {
+            livereload: {
+                host: 'localhost',
+                files: [
+                    './index.html',
+                    './assets/styles/*.scss',
+                    './assets/js/*.js'
+                ],
+                open: true
             }
         }
-    
-        });
-    
+    },
 
-        grunt.loadNpmTasks('grunt-contrib-watch');
-        grunt.loadNpmTasks('grunt-contrib-sass');
-        grunt.loadNpmTasks('grunt-contrib-uglify');
-        grunt.loadNpmTasks('grunt-contrib-concat');
-        grunt.loadNpmTasks('grunt-contrib-copy');
+    sass: {
+        dist: {
+            files: {
+                'dist/build/styles/style.css' : 'assets/styles/base.scss'
+            }
+        }
+    },
 
-        grunt.registerTask('build', ['uglify', 'sass', 'copy']);
-        grunt.registerTask('dev', ['uglify', 'sass', 'copy', 'watch']);
-    };
+    uglify:{
+        options: {
+            manage: false,
+            preserveComments: 'all'
+        },
+        my_target:{
+            files: {
+                'dist/build/scripts/main.min.js' : ['assets/js/*.js']
+            }
+        }
+    },
+    copy: {
+        main: {
+            expand: true,
+            cwd: 'assets/img/',
+            src: '*.{png,jpg,svg}',
+            dest: 'dist/build/images'
+        }
+    }    
+});
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('build', ['uglify', 'sass', 'copy']);
+    grunt.registerTask('dev', ['uglify', 'sass', 'copy', 'watch']);
+};
--- search/assets/js/main.js
+++ search/assets/js/main.js
@@ -1,3 +1,112 @@
+function fetchData() {
+	return fetch('https://dog.ceo/api/breeds/list')
+		.then(function(response) {  
+			if (response.status !== 200) {  
+				console.log('Looks like there was a problem. Status Code: ' +  response.status);  
+				return;  
+			}
+
+			return response.json().then(function(json) {
+				return json.message;
+			});
+		}  
+	)  
+	.catch(function(err) {  
+		console.log('Error:', err);  
+	});
+}
+
+function filterMatchingResults(input, data) {
+	return data.filter(function(word) {
+		return word.toLowerCase().includes(input.value.toLowerCase());
+	})
+}
+
+function generateSuggestions(matches) {
+	return matches.map(function(match) {return '<div class="searchSuggestion">' + match + '</div>'});
+}
+
+function getResults(event, input, data) {
+	var matches = filterMatchingResults(input, data);
+	var suggestions = generateSuggestions(matches);
+
+    if(input.value.length > 0) {
+		$('.searchSuggestions').addClass('visible');
+		$('.searchSuggestions').html(suggestions);
+		$('.searchSuggestions').children('.searchSuggestion').first().addClass('active');
+    } else {
+        $('.searchSuggestions').html('');
+        $('.searchSuggestions').removeClass('visible');
+    }
+}
+
+function addSearchTerm(term) {
+	if(term.length > 0) {
+		$('.previousSearches').addClass('visible');
+		$('.previousSearches').append(
+			'<div class="previousSearch"><div class="previousSearchTerm"><p>' + term + '</p><p>' + moment().format('YYYY-MM-DD hh:mm') + '</p></div><div class="removeSearch"><div class="removeCircle">x</div></div></div>'
+		);
+	}
+
+	resetSearchField();
+}
+
+function resetSearchField() {
+	$('#searchField').val('');
+	$('.searchSuggestions').html('');
+	$('.searchSuggestions').removeClass('visible');
+}
+
+function removeSearchTerm(search) {
+	$(search).remove();
+}
+
 $(document).ready(function() {
-    console.log('ready');
+	fetchData()
+		.then(function(searchData) {
+			$('#searchField').on('input', function(event) {
+				getResults(event, this, searchData)
+			});
+			
+			$('#searchField').on('keydown', function(event) {
+				if(event.keyCode === 40) {
+					var currentSuggestions = $('.searchSuggestions').children('.searchSuggestion');
+					var activeIndex = $('.active').index('.searchSuggestion');
+
+					if(activeIndex < 0) {
+						currentSuggestions.first().addClass('active');
+					} else if(activeIndex !== (currentSuggestions.length - 1)) {
+						$(currentSuggestions[activeIndex]).removeClass('active');
+						$(currentSuggestions[activeIndex + 1]).addClass('active');
+					}
+				} else if(event.keyCode === 38) {
+					var currentSuggestions = $('.searchSuggestions').children('.searchSuggestion');
+					var activeIndex = $('.active').index('.searchSuggestion');
+
+					if(activeIndex > 0) {
+						$(currentSuggestions[activeIndex]).removeClass('active');
+						$(currentSuggestions[activeIndex - 1]).addClass('active');
+					}
+				} else if(event.keyCode === 13) {
+					var currentSuggestions = $('.searchSuggestions').children('.searchSuggestion');
+					var activeIndex = $('.active').index('.searchSuggestion');
+
+					addSearchTerm($(currentSuggestions[activeIndex]).text());
+				}
+			});
+
+			$('.searchSuggestions').on('mouseenter', '.searchSuggestion', function(event) {
+				$('.searchSuggestion').removeClass('active');
+				$(this).addClass('active');
+			});
+
+			$('.searchSuggestions').on('click', '.searchSuggestion', function(event) {
+				addSearchTerm($(this).text());
+				resetSearchField();
+			})
+
+			$('.previousSearches').on('click', '.removeSearch', function(event) {
+				removeSearchTerm($(this).closest('.previousSearch'));
+			});
+		});
 });
